198bcd325a07ff9399b9aaeba696a9c759f5b260
Matti Vesa <mattijvesa@gmail.com>
2020-01-05 18:24:36 +0200

    Fix electron script, bump version to 0.3.0

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { exec } = require("child_process");
-exec("electron " + __dirname, (err, stdout, stderr) => {
+exec("electron " + __dirname + "/index.js", (err, stdout, stderr) => {
   if (err) {
     console.log("Could not execute electron. Reason:");
     console.log(stderr);
--- public/extension.js
+++ public/extension.js
@@ -322,7 +322,7 @@ const getHTML = (folder, state) => {
 
   <!-- Header fields and buttons-->
   <div id="design-details">
-    <span>Unide 0.2.1</span>
+    <span>Unide 0.3.0</span>
     <button id="project-settings">Settings</button>
     <button id="new-design">New Design</button>
     <button id="sketch-design">Sketch</button>
